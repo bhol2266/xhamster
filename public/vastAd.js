b6ba2034dd67969d290c1306855d880eaf909358
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-09-07 00:06:41 +0545

    asdfadsf

--- public/vastAd.js
+++ public/vastAd.js
@@ -148,7 +148,7 @@ function autoplayChecksResolved() {
 
   // Request video ads.
   const adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl = 'https://anewfeedliberty.com/ceef/gdt3g0/tbt/2051944/tlk.xml'; //ClickAdu 
+  adsRequest.adTagUrl = 'https://anewfeedliberty.com/ceef/gdt3g0/tbt/2051944/tlk.xml...'; //ClickAdu 
 
   // Specify the linear and nonlinear slot sizes. This helps the SDK to
   // select the correct creative if multiple are returned.
